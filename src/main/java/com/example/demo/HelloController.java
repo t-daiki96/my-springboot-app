12a1df86176fb1dcfdd667e74345faeb7801fdc8
t-daiki96 <bvbdoru@gmail.com>
2025-06-22 16:34:02 +0900

    change text

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, world!";
+        return "Hello, world!!!!!!!!!!!";
     }
 }
 
